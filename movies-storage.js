308af1f879754de78e53567a88b079a4bbad1bc6
MKL592 <artur-panasiuk592@wp.pl>
2021-02-24 22:54:05 +0100

    comments on future code

--- movies-storage.js
+++ movies-storage.js
@@ -1,3 +1,30 @@
-class MoviesStorage{
-    
+export default class MoviesStorage{
+    constructor(){
+        if(localStorage.getItem('movies') && localStorage.getItem('movies') === Array){
+            //stays empty for now
+            //console.log("Item exists in local storage")
+        }else{
+            //create default list on localstorage with defaultData.json
+        }
+    }
+
+    get(id){
+        if(id !== undefined){
+            //get movie with specific id
+        }else{
+            //get all movies from list
+        }
+    }
+
+    set(id, data){
+        if(data !== undefined){
+            //update movie with input id with data
+        }else{
+            //add new movie to list
+        }
+    }
+
+    remove(id){
+        //remove movie with input id
+    }
 }

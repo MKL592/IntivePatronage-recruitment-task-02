d72ff294699fb849534265bf0b15b44f9349af47
MKL592 <artur-panasiuk592@wp.pl>
2021-02-24 22:42:37 +0100

    commenting future code

---
+++ add.js
@@ -0,0 +1,6 @@
+//Create logic for:
+//1.Cannot add movie without: title, year and genre
+//2.Cannot add movie if year is not 4 digit number
+//3.Cannot add movie if title is not unique
+//4.Clean user input after passing data
+//5.If error accures, output reason

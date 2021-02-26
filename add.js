2757d216f4ca6c4840d75f19339eed2b1d406f21
MKL592 <artur-panasiuk592@wp.pl>
2021-02-26 01:28:42 +0100

    Debug asap

--- add.js
+++ add.js
@@ -4,3 +4,33 @@
 //3.Cannot add movie if title is not unique
 //4.Clean user input after passing data
 //5.If error accures, output reason
+
+function validateInput(){
+    let userInput = {
+        "title": document.getElementById("title").value,
+        "year": document.getElementById("year").value,
+        "genre": document.getElementById("genre").value,
+        "summary": document.getElementById("summary").value
+    }
+    console.log(userInput.title);
+    if(userInput.title != "" || userInput.year != "" || userInput.genre != ""){
+        let currentYear = new Date();
+        let getLocalInfo = localStorage.getItem("movies");
+        let iteration = 0;
+        for(iteration in getLocalInfo){
+            if(getLocalInfo[iteration].title == userInput.title){
+                errorHandler("Title already exists!");
+                break;
+            }
+        }
+
+        if(userInput.year > currentYear.getFullYear() || userInput.year < 999){
+            errorHandler("Invalid year!");
+        }
+        
+    }
+}
+
+function errorHandler(errorMessage){
+    alert(errorMessage);
+}
